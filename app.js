20a4046be894c38ba39511f2e2ca345a39d8538c
Elias Casa <eliasaaronalfaro@gmail.com>
2026-03-26 16:54:38 -0300

    microservicio parcial devops

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Microservicio funcionando 🚀");
+});
+
+app.get('/mensaje', (req, res) => {
+    res.json({
+        mensaje: "Hola desde el backend"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
+});
